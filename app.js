1d92ce9c36598f3293332cce7e0d20d1e9898345
Warren Christopher A. Quiruben <WARRENCHRISTOPHER.QUIRUBEN@live.ama.edu.ph>
2025-09-01 11:04:50 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,243 +1,43 @@
+// This example focuses on Widevine channels only.
+// Make sure you have Shaka Player loaded and your video element is <video id="video"></video>
+
 const channels = {
   "ALLTV": {
     name: "ALLTV",
     url: "http://143.44.136.110:6610/001/2/ch00000090990000001179/manifest.mpd?virtualDomain=001.live_hls.zte.com",
-    keys: {
-      "license_type": "com.widevine.alpha",
-      "license_key": "http://143.44.136.74:9443/widevine/?deviceId=02:00:00:00:00:00"
+    drm: {
+      "com.widevine.alpha": {
+        serverURL: "http://143.44.136.74:9443/widevine/?deviceId=02:00:00:00:00:00"
+      }
     }
   },
   "DWIZ_News_TV": {
     name: "DWIZ News TV",
     url: "http://143.44.136.110:6610/001/2/ch00000090990000001109/manifest.mpd?virtualDomain=001.live_hls.zte.com",
-    keys: {
-      "license_type": "com.widevine.alpha",
-      "license_key": "http://143.44.136.74:9443/widevine/?deviceId=02:00:00:00:00:00"
+    drm: {
+      "com.widevine.alpha": {
+        serverURL: "http://143.44.136.74:9443/widevine/?deviceId=02:00:00:00:00:00"
+      }
     }
   },
   "ANC": {
     name: "ANC",
     url: "http://143.44.136.110:6610/001/2/ch00000090990000001274/manifest.mpd?virtualDomain=001.live_hls.zte.com",
-    keys: {
-      "license_type": "com.widevine.alpha",
-      "license_key": "http://143.44.136.74:9443/widevine/?deviceId=02:00:00:00:00:00"
+    drm: {
+      "com.widevine.alpha": {
+        serverURL: "http://143.44.136.74:9443/widevine/?deviceId=02:00:00:00:00:00"
+      }
     }
   },
-  "Bilyonaryo_News_Channel": {
-    name: "Bilyonaryo News Channel",
-    url: "https://qp-pldt-live-grp-05-prod.akamaized.net/out/u/bilyonaryoch.mpd",
-    keys: {
-      "license_type": "clearkey",
-      "license_key": "227ffaf09bec4a889e0e0988704d52a2:b2d0dce5c486891997c1c92ddaca2cd2"
-    }
-  },
-  "CLTV_36": {
-    name: "CLTV 36",
-    url: "http://143.44.136.110:6610/001/2/ch00000090990000001314/manifest.mpd?virtualDomain=001.live_hls.zte.com",
-    keys: {
-      "license_type": "com.widevine.alpha",
-      "license_key": "http://143.44.136.74:9443/widevine/?deviceId=02:00:00:00:00:00"
-    }
-  },
-  "Celestial_Movies_Pinoy": {
-    name: "Celestial Movies Pinoy",
-    url: "http://143.44.136.113:6910/001/2/ch00000090990000001077/manifest.mpd?virtualDomain=001.live_hls.zte.com",
-    keys: {
-      "license_type": "com.widevine.alpha",
-      "license_key": "http://143.44.136.74:9443/widevine/?deviceId=02:00:00:00:00:00"
-    }
-  },
-  "DZMM_Teleradyo": {
-    name: "DZMM Teleradyo",
-    url: "http://143.44.136.110:6610/001/2/ch00000090990000001249/manifest.mpd?virtualDomain=001.live_hls.zte.com",
-    keys: {
-      "license_type": "com.widevine.alpha",
-      "license_key": "http://143.44.136.74:9443/widevine/?deviceId=02:00:00:00:00:00"
-    }
-  },
-  "DZRH_TV": {
-    name: "DZRH TV",
-    url: "http://143.44.136.110:6610/001/2/ch00000090990000001174/manifest.mpd?virtualDomain=001.live_hls.zte.com",
-    keys: {
-      "license_type": "com.widevine.alpha",
-      "license_key": "http://143.44.136.74:9443/widevine/?deviceId=02:00:00:00:00:00"
-    }
-  },
-  "NET25": {
-    name: "NET25",
-    url: "http://143.44.136.111:6910/001/2/ch00000090990000001090/manifest.mpd?virtualDomain=001.live_hls.zte.com",
-    keys: {
-      "license_type": "com.widevine.alpha",
-      "license_key": "http://143.44.136.74:9443/widevine/?deviceId=02:00:00:00:00:00"
-    }
-  },
-  "Radyo_Bandido_TV": {
-    name: "Radyo Bandido TV",
-    url: "http://143.44.136.110:6610/001/2/ch00000090990000001338/manifest.mpd?virtualDomain=001.live_hls.zte.com",
-    keys: {
-      "license_type": "com.widevine.alpha",
-      "license_key": "http://143.44.136.74:9443/widevine/?deviceId=02:00:00:00:00:00"
-    }
-  },
-  "RJTV": {
-    name: "RJTV",
-    url: "http://143.44.136.110:6610/001/2/ch00000090990000001159/manifest.mpd?virtualDomain=001.live_hls.zte.com",
-    keys: {
-      "license_type": "com.widevine.alpha",
-      "license_key": "http://143.44.136.74:9443/widevine/?deviceId=02:00:00:00:00:00"
-    }
-  },
-  "ROCK_MNL": {
-    name: "ROCK of MANILA",
-    url: "http://143.44.136.110:6610/001/2/ch00000090990000001177/manifest.mpd?virtualDomain=001.live_hls.zte.com",
-    keys: {
-      "license_type": "com.widevine.alpha",
-      "license_key": "http://143.44.136.74:9443/widevine/?deviceId=02:00:00:00:00:00"
-    }
-  },
-  "Knowledge_Channel": {
-    name: "Knowledge Channel",
-    url: "http://143.44.136.110:6610/001/2/ch00000090990000001340/manifest.mpd?virtualDomain=001.live_hls.zte.com",
-    keys: {
-      "license_type": "com.widevine.alpha",
-      "license_key": "http://143.44.136.74:9443/widevine/?deviceId=02:00:00:00:00:00"
-    }
-  },
-  "Pinoy_Xtreme": {
-    name: "Pinoy Xtreme",
-    url: "http://143.44.136.110:6610/001/2/ch00000090990000001098/manifest.mpd?virtualDomain=001.live_hls.zte.com",
-    keys: {
-      "license_type": "com.widevine.alpha",
-      "license_key": "http://143.44.136.74:9443/widevine/?deviceId=02:00:00:00:00:00"
-    }
-  },
-  "SolarFlix": {
-    name: "SolarFlix",
-    url: "http://143.44.136.111:6910/001/2/ch00000090990000001243/manifest.mpd?virtualDomain=001.live_hls.zte.com",
-    keys: {
-      "license_type": "com.widevine.alpha",
-      "license_key": "http://143.44.136.74:9443/widevine/?deviceId=02:00:00:00:00:00"
-    }
-  },
-  "UNTV": {
-    name: "UNTV",
-    url: "http://143.44.136.111:6910/001/2/ch00000090990000001091/manifest.mpd?virtualDomain=001.live_hls.zte.com",
-    keys: {
-      "license_type": "com.widevine.alpha",
-      "license_key": "http://143.44.136.74:9443/widevine/?deviceId=02:00:00:00:00:00"
-    }
-  },
-  "Kapamilya_Channel_SD": {
-    name: "Kapamilya Channel SD",
-    url: "http://143.44.136.110:6610/001/2/ch00000090990000001248/manifest.mpd?virtualDomain=001.live_hls.zte.com",
-    keys: {
-      "license_type": "com.widevine.alpha",
-      "license_key": "http://143.44.136.74:9443/widevine/?deviceId=02:00:00:00:00:00"
-    }
-  },
-  "GMA": {
-    name: "GMA",
-    url: "http://143.44.136.110:6610/001/2/ch00000090990000001093/manifest.mpd?virtualDomain=001.live_hls.zte.com",
-    keys: {
-      "license_type": "com.widevine.alpha",
-      "license_key": "http://143.44.136.74:9443/widevine/?deviceId=02:00:00:00:00:00"
-    }
-  },
-  "GTV": {
-    name: "GTV",
-    url: "http://143.44.136.110:6610/001/2/ch00000090990000001143/manifest.mpd?virtualDomain=001.live_hls.zte.com",
-    keys: {
-      "license_type": "com.widevine.alpha",
-      "license_key": "http://143.44.136.74:9443/widevine/?deviceId=02:00:00:00:00:00"
-    }
-  },
-  "PTV4": {
-    name: "PTV 4",
-    url: "https://qp-pldt-live-grp-02-prod.akamaized.net/out/u/cg_ptv4_sd.mpd",
-    keys: {
-      "license_type": "clearkey",
-      "license_key": "71a130a851b9484bb47141c8966fb4a3:ad1f003b4f0b31b75ea4593844435600"
-    }
-  },
-  "A2Z": {
-    name: "A2Z",
-    url: "https://qp-pldt-live-grp-02-prod.akamaized.net/out/u/cg_a2z.mpd",
-    keys: {
-      "license_type": "clearkey",
-      "license_key": "f703e4c8ec9041eeb5028ab4248fa094:c22f2162e176eee6273a5d0b68d19530"
-    }
-  },
-  "Sari_Sari": {
-    name: "Sari-Sari",
-    url: "https://qp-pldt-live-grp-06-prod.akamaized.net/out/u/cg_sari_sari_sd.mpd",
-    keys: {
-      "license_type": "clearkey",
-      "license_key": "0a7ab3612f434335aa6e895016d8cd2d:b21654621230ae21714a5cab52daeb9d"
-    }
-  },
-  "One_PH": {
-    name: "One PH",
-    url: "https://qp-pldt-live-grp-04-prod.akamaized.net/out/u/oneph_sd.mpd",
-    keys: {
-      "license_type": "clearkey",
-      "license_key": "92834ab4a7e1499b90886c5d49220e46:a7108d9a6cfcc1b7939eb111daf09ab3"
-    }
-  },
-  "Cinema_One": {
-    name: "Cinema One",
-    url: "http://143.44.136.110:6610/001/2/ch00000090990000001283/manifest.mpd?virtualDomain=001.live_hls.zte.com",
-    keys: {
-      "license_type": "com.widevine.alpha",
-      "license_key": "http://143.44.136.74:9443/widevine/?deviceId=02:00:00:00:00:00"
-    }
-  },
-  "Cinemo": {
-    name: "Cinemo",
-    url: "http://143.44.136.110:6610/001/2/ch00000090990000001254/manifest.mpd?virtualDomain=001.live_hls.zte.com",
-    keys: {
-      "license_type": "com.widevine.alpha",
-      "license_key": "http://143.44.136.74:9443/widevine/?deviceId=02:00:00:00:00:00"
-    }
-  },
-  "Jeepney_TV": {
-    name: "Jeepney TV",
-    url: "http://143.44.136.113:6910/001/2/ch00000090990000001250/manifest.mpd?virtualDomain=001.live_hls.zte.com",
-    keys: {
-      "license_type": "com.widevine.alpha",
-      "license_key": "http://143.44.136.74:9443/widevine/?deviceId=02:00:00:00:00:00"
-    }
-  },
-  "CinemaWorld": {
-    name: "CinemaWorld",
-    url: "http://143.44.136.113:6910/001/2/ch00000090990000001158/manifest.mpd?virtualDomain=001.live_hls.zte.com",
-    keys: {
-      "license_type": "com.widevine.alpha",
-      "license_key": "http://143.44.136.74:9443/widevine/?deviceId=02:00:00:00:00:00"
-    }
-  },
-  "TAP_EDGE": {
-    name: "TAP EDGE",
-    url: "http://143.44.136.110:6610/001/2/ch00000090990000001150/manifest.mpd?virtualDomain=001.live_hls.zte.com",
-    keys: {
-      "license_type": "com.widevine.alpha",
-      "license_key": "http://143.44.136.74:9443/widevine/?deviceId=02:00:00:00:00:00"
-    }
-  },
-  "Asian_Food_Network": {
-    name: "Asian Food Network",
-    url: "http://143.44.136.113:6910/001/2/ch00000090990000001342/manifest.mpd?virtualDomain=001.live_hls.zte.com",
-    keys: {
-      "license_type": "com.widevine.alpha",
-      "license_key": "http://143.44.136.74:9443/widevine/?deviceId=02:00:00:00:00:00"
-    }
-  },
-  // ... All remaining channels from your context would be listed here in the same format ...
+  // ... Add more Widevine channels in this format ...
 };
+
+// Select elements
 const selectMobile = document.getElementById('channelSelect');
 const selectDesktop = document.getElementById('channelSelectDesktop');
 const video = document.getElementById('video');
 
-// Helper to populate a select element with channels
+// Helper to populate channels in selects
 function populateChannels(select) {
   select.innerHTML = '';
   const defaultOption = document.createElement('option');
@@ -252,13 +52,12 @@ function populateChannels(select) {
   }
 }
 
-// Populate both selectors
 populateChannels(selectMobile);
 populateChannels(selectDesktop);
 
 let player = null;
 
-// Load channel helper
+// Load channel helper for Widevine
 async function loadChannel(channelKey) {
   const channel = channels[channelKey];
   if (!channel) return;
@@ -274,15 +73,11 @@ async function loadChannel(channelKey) {
     console.error('Error code', e.detail.code, 'object', e.detail);
   });
 
-  const drmConfig = {};
-
-  if (channel.keys) {
-    drmConfig.clearKeys = channel.keys;
-  }
-
-  if (channel.license && channel.license.type && channel.license.url) {
+  // DRM config for Widevine
+  let drmConfig = {};
+  if (channel.drm && channel.drm["com.widevine.alpha"] && channel.drm["com.widevine.alpha"].serverURL) {
     drmConfig.servers = {
-      [channel.license.type]: channel.license.url
+      "com.widevine.alpha": channel.drm["com.widevine.alpha"].serverURL
     };
   }
 
@@ -320,11 +115,13 @@ function onChannelChange(e) {
 selectMobile.addEventListener('change', onChannelChange);
 selectDesktop.addEventListener('change', onChannelChange);
 
-// Load default channel (GMA PINOY TV) on page load
+// Load default channel on page load
 window.addEventListener('DOMContentLoaded', () => {
-  const defaultChannelKey = 'gma_pinoy_tv';
-  selectMobile.value = defaultChannelKey;
-  selectDesktop.value = defaultChannelKey;
-  loadChannel(defaultChannelKey);
+  const keys = Object.keys(channels);
+  if (keys.length) {
+    const defaultChannelKey = keys[0];
+    selectMobile.value = defaultChannelKey;
+    selectDesktop.value = defaultChannelKey;
+    loadChannel(defaultChannelKey);
+  }
 });
-
